da8d8c8fbe13cf5376877a32f7c4a971e588345c
gbhat <gbhatweb@gmail.com>
2021-05-24 17:34:48 +0530

    gradient_descent_with_momentum.py and gradient_descent_with_momentum2.py
    Signed-off-by: gbhat <gbhatweb@gmail.com>

--- gradient_descent_with_momentum2.py
+++ gradient_descent_with_momentum2.py
@@ -3,19 +3,19 @@ import numpy as np
 
 
 def f(x):
-    return x ** 2
+    return x ** 2 * tf.math.sin(x)
 
 
-sgd_opt = tf.keras.optimizers.SGD(learning_rate=0.1)
-sgd_with_momentum_opt = tf.keras.optimizers.SGD(learning_rate=0.1, momentum=0.95)
+sgd_opt = tf.keras.optimizers.SGD(learning_rate=0.01)
+sgd_with_momentum_opt = tf.keras.optimizers.SGD(learning_rate=0.01, momentum=0.95)
 
-tfx = tf.Variable(10.0)
+tfx = tf.Variable(-4.87)
 for i in range(0, 1000):
     sgd_opt.minimize(lambda: f(tfx), var_list=[tfx])
 
 print(" Minimum value with Gradient Descent : (", np.round(tfx.numpy(), 3), ",", np.round(f(tfx).numpy(), 3), ")")
 
-tfx = tf.Variable(10.0)
+tfx = tf.Variable(-4.87)
 for i in range(0, 1000):
     sgd_with_momentum_opt.minimize(lambda: f(tfx), var_list=[tfx])
 
