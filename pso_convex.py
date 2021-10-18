74a9883ac8b3864111fcb31fc881a3d362a03496
gbhat <gbhatweb@gmail.com>
2021-10-18 23:12:13 +0530

    pso_convex.py
    Signed-off-by: gbhat <gbhatweb@gmail.com>

--- pso_convex.py
+++ pso_convex.py
@@ -1,1 +1,15 @@
-# Coming soon...
+import numpy as np
+import pyswarms as ps
+
+
+def f(x):
+    x_ = x[:, 0]
+    y_ = x[:, 1]
+    return x_ ** 2 + y_ ** 2
+
+
+options = {'c1': 0.5, 'c2': 1.2, 'w': 0.9}
+bounds = np.array([[-2, -1], [2, 1]])
+optimizer = ps.single.GlobalBestPSO(n_particles=100, dimensions=2, bounds=bounds, options=options)
+best_cost, best_pos = optimizer.optimize(f, iters=100)
+print("Best position: ", best_pos)
