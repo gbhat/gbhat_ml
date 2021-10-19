2ab171768d68828f68bd68e73bb0192efb03653e
gbhat <gbhatweb@gmail.com>
2021-10-19 19:45:12 +0530

    Add rounding for best positions
    Signed-off-by: gbhat <gbhatweb@gmail.com>

--- pso_convex.py
+++ pso_convex.py
@@ -12,4 +12,5 @@ options = {'c1': 0.5, 'c2': 1.2, 'w': 0.9}
 bounds = np.array([[-2, -1], [2, 1]])
 optimizer = ps.single.GlobalBestPSO(n_particles=100, dimensions=2, bounds=bounds, options=options)
 best_cost, best_pos = optimizer.optimize(f, iters=100)
+best_pos = np.round(best_pos, 2)        # Round to 2 decimals
 print("Best position: ", best_pos)
--- pso_nonconvex.py
+++ pso_nonconvex.py
@@ -12,4 +12,5 @@ options = {'c1': 0.25, 'c2': 1.2, 'w': 0.9}
 bounds = np.array([[-3, -3], [3, 3]])
 optimizer = ps.single.GlobalBestPSO(n_particles=100, dimensions=2, bounds=bounds, options=options)
 best_cost, best_pos = optimizer.optimize(f, iters=250)
+best_pos = np.round(best_pos, 2)        # Round to 2 decimals
 print("Best position: ", best_pos)
